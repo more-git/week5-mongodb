efa50f60e636dd33f257953e4e3689c66c22d323
more-git <57333225+more-git@users.noreply.github.com>
2020-09-29 12:03:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,46 +6,41 @@ var MongoClient = require('mongodb').MongoClient;
 
 connectDB();
 
-function addObject(collection, object){
-    if (object && collection) {
-        collection.insert(object, function(err, result){
-            if(!err){
-                console.log("Inserted : ");
-                console.log(result);
+function connectDB() {
+    http.createServer(function (req, res) {
+        var urlObj = url.parse(req.url, true, false);
+        fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
+            if (err) {  }
+            if (req.method == "POST") {
+                MongoClient.connect("mongodb://localhost/",{useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
+                    var myDB = db.db("week5");
+                    var inputCollection = myDB.collection("input");
+
+                    var jsonData = "";
+                    req.on('data', function (chunk) {
+                        jsonData += chunk;
+                    });
+                    req.on('end', function () {
+                        if (jsonData) {
+                            var len = jsonData.toString().length;
+                            var substrMsg = jsonData.toString().substr(8, len - 1);
+                            var text = '{ "message":"';
+                            text += substrMsg;
+                            text += '"}';
+                            var obj = JSON.parse(text, function (key, value) {
+                                return value;
+                            });
+                            inputCollection.insertOne(obj);
+                        }
+                    });
+                    res.writeHead(200);
+                    res.end(data);
+                });
+            } else {
+                if (err) {  }
+                res.writeHead(200);
+                res.end(data);
             }
         });
-    }
-}
-function connectDB() {
-    MongoClient.connect("mongodb://localhost/",{useNewUrlParser: true, useUnifiedTopology: true}, function(err, db) {
-        var myDB = db.db("week5");
-        myDB.dropCollection("input");
-        myDB.createCollection("input", function(err, nebulae){
-            http.createServer(function (req, res) {
-                var urlObj = url.parse(req.url, true, false);
-                fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
-                    if (req.method == "POST") {
-                        if (err) {  }
-                        var jsonData = "";
-                        req.on('data', function (chunk) {
-                            jsonData += chunk;
-                        });
-                        req.on('end', function () {
-                            if (jsonData) {
-                               var len = jsonData.toString().length;
-                               var substrMsg = jsonData.toString().substr(8, len-1);
-                               addObject(nebulae, {message:substrMsg});
-                            }
-                        });
-                        res.writeHead(200);
-                        res.end(data);
-                    } else {
-                        if (err) {  }  
-                        res.writeHead(200);
-                        res.end(data);  
-                  }
-                });
-            }).listen(8005);
-        });
-    });
+    }).listen(8005);
 }
